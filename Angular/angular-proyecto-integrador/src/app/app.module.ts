99597872ac393b4bf0b72c168e913084d135411f
antomiculic <antonellamiculic@gmail.com>
2023-02-02 21:44:47 -0300

    commit 3

--- Angular/angular-proyecto-integrador/src/app/app.module.ts
+++ Angular/angular-proyecto-integrador/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ExperienceComponent } from './components/experience/experience.componen
 import { EducationComponent } from './components/education/education.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +25,9 @@ import { HysComponent } from './components/hys/hys.component';
     AcercaDeComponent,
     ExperienceComponent,
     EducationComponent,
-    HysComponent
+    HysComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
